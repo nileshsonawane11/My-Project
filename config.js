3c5f549280ae41fe0be3fe90458a82f60871bd9a
nileshsonawane11 <sonawanenileshk6@gmail.com>
2025-09-14 14:26:47 +0530

    Basketball Done

--- config.js
+++ config.js
@@ -1,3 +1,3 @@
 // config.js
-export const host = "localhost";  // Required for Render/Heroku/etc.
-export const port =  8080; // Render gives port via env
+export const host = "0.0.0.0"; // Required for Render/Heroku/etc.
+export const port = process.env.PORT || 8080; // Render gives port via env
